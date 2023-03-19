a69abe5407d57b41731e768b955b8e9f32011f6b
Chstphr <somto565@gmail.com>
2023-03-19 10:45:57 +0100

    added home jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section class="home" id="home">
+      <div class="home-text">
+        <h3>Hello I'm</h3>
+        <h1>Chrstphr</h1>
+        <h5>A Creative Developer <span>From Enugu</span></h5>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti
+          nostrum quam <br />
+          facilis corporis ab exercitationem?
+        </p>
+        <div class="social">
+          <a href="#"><i class="bx bxl-facebook"></i></a>
+          <a href="#"><i class="bx bxl-twitter"></i></a>
+          <a href="#"><i class="bx bxl-linkedin"></i></a>
+          <a href="#"><i class="bx bxl-behance"></i></a>
+        </div>
+        <a href="#" class="btn">About Me</a>
+      </div>
+
+      <div class="home-img">
+        <img src="img/main.png" alt="" />
+      </div>
+    </section>
   )
 }
 
