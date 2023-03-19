58bd09c2796a59ad5ada3c436617c74576f69854
Chstphr <somto565@gmail.com>
2023-03-19 10:23:36 +0100

    added and imported imgs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React from "react";
+import Logo from '../assets/img/logo.png'
 
 const Navbar = () => {
   return (
     <header>
-      <a href="#" class="logo"><img src="img/logo.png" /></a>
+      <a href="#" class="logo">
+        <img src={Logo} />
+      </a>
       <div class="bx bx-menu" id="menu-icon"></div>
 
       <ul class="navlist">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#service">Service</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#service">Service</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
-      <a href="#" class="top-btn">Download CV</a>
+      <a href="#" class="top-btn">
+        Download CV
+      </a>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
