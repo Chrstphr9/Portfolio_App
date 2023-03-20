f35a223ba29afd8b4616301e337e38acf5b5dfda
Chstphr <somto565@gmail.com>
2023-03-20 17:16:36 +0100

    working on portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React from "react";
 
 const Portfolio = () => {
   return (
-    <div></div>
-  )
-}
+    <section class="portfolio" id="portfolio">
+      <div class="heading">
+        <p>Portfolio</p>
+        <h2>
+          <span>Latest</span> Project
+        </h2>
+      </div>
+      <div class="portfolio-content">
+        <div class="row">
+          <img src="img/work1.jpg" alt="" />
+          <div class="layer">
+            <h5>Visual Design</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. At
+              nostrum rem, doloribus neque atque hic.
+            </p>
+            <a href="#">
+              <i class="bx bx-link-external"></i>
+            </a>
+          </div>
+        </div>
 
-export default Portfolio
+        <div class="row">
+          <img src="img/work2.jpg" alt="" />
+          <div class="layer">
+            <h5>Visual Design</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. At
+              nostrum rem, doloribus neque atque hic.
+            </p>
+            <a href="#">
+              <i class="bx bx-link-external"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="row">
+          <img src="img/work3.jpg" alt="" />
+          <div class="layer">
+            <h5>Visual Design</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. At
+              nostrum rem, doloribus neque atque hic.
+            </p>
+            <a href="#">
+              <i class="bx bx-link-external"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="row">
+          <img src="img/work4.jpg" alt="" />
+          <div class="layer">
+            <h5>Visual Design</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. At
+              nostrum rem, doloribus neque atque hic.
+            </p>
+            <a href="#">
+              <i class="bx bx-link-external"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="row">
+          <img src="img/work5.jpg" alt="" />
+          <div class="layer">
+            <h5>Visual Design</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. At
+              nostrum rem, doloribus neque atque hic.
+            </p>
+            <a href="#">
+              <i class="bx bx-link-external"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="row">
+          <img src="img/work6.jpg" alt="" />
+          <div class="layer">
+            <h5>Visual Design</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. At
+              nostrum rem, doloribus neque atque hic.
+            </p>
+            <a href="#">
+              <i class="bx bx-link-external"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
