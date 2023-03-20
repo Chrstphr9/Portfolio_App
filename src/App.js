5995bb2ba2c2e34b9747f2da6251a602912f0dd0
Chstphr <somto565@gmail.com>
2023-03-20 16:25:27 +0100

    changed the service name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Services from './components/Services';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <Services />
     </div>
   );
 }
