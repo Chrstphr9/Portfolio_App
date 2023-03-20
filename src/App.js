3306813bdf16a2973601279f3fd5c8f7e47385f6
Chstphr <somto565@gmail.com>
2023-03-20 21:05:12 +0100

    imported service2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Services from './components/Services';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import Service2 from './components/Service2';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Services />
       <About />
       <Portfolio />
+      <Service2 />
     </div>
   );
 }
