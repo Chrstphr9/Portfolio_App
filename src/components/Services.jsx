b0ac8a20855329fa0cf1e8cc5be7b146146b4ea9
Chstphr <somto565@gmail.com>
2023-03-20 16:46:24 +0100

    done with service css

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Target from '../assets/img/target.svg'
+import Brush from '../assets/img/brush.svg'
+import Energy from '../assets/img/energy.svg'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <seciton class="sub-service">
+      <div class="items">
+        <div class="sub-box">
+          <div class="sub-img">
+            <img src={Target} alt="" />
+          </div>
+          <h3>Pixel Perfect</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            tenetur incidunt atque fugit impedit molestiae iure.
+          </p>
+        </div>
+
+        <div class="sub-box">
+          <div class="sub-img">
+            <img src={Brush} alt="" />
+          </div>
+          <h3>High Quality</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            tenetur incidunt atque fugit impedit molestiae iure.
+          </p>
+        </div>
+
+        <div class="sub-box">
+          <div class="sub-img">
+            <img src={Energy} alt="" />
+          </div>
+          <h3>Awesome Idea</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            tenetur incidunt atque fugit impedit molestiae iure.
+          </p>
+        </div>
+      </div>
+    </seciton>
   )
 }
 
