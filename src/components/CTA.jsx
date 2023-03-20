76c358fc3ba07839013d7ecfd2fd350742b70603
Chstphr <somto565@gmail.com>
2023-03-20 21:10:00 +0100

    wrote CTA jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section class="cta">
+      <div class="heading">
+        <h2>Completed A Few Projects <br />Successfully</h2>
+      </div>
+
+      <div class="cta-box">
+        <div class="wrap one">
+          <h3>#####</h3>
+          <p>Finished Projects</p>
+        </div>
+
+        <div class="wrap two">
+          <h3>#####</h3>
+          <p>Finished Projects</p>
+        </div>
+
+        <div class="wrap three">
+          <h3>#####</h3>
+          <p>Finished Projects</p>
+        </div>
+      </div>
+    </section>
+
   )
 }
 
