b848c173055daab969a3d986af6d4d2060473c08
Chstphr <somto565@gmail.com>
2023-03-20 20:54:07 +0100

    done with service2 jsx

--- src/components/Service2.jsx
+++ src/components/Service2.jsx
@@ -2,7 +2,70 @@ import React from 'react'
 
 const Service2 = () => {
   return (
-    
+    <section class="service" id="service">
+      <div class="heading">
+        <h3>Service</h3>
+        <h2>What I Do For Clients</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos odio
+          nobis incidunt, laudantium officiis vel.
+        </p>
+      </div>
+
+      <div class="service-content">
+        <div class="rows">
+          <div class="s s-one">
+            <img src="img/anchor.svg" alt="" />
+          </div>
+          <h3>WEb DeVelOpmEnT</h3>
+          <h5>Start From <span>$99</span></h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+            maiores ipsum, neque quae quo velit similique perferendis, ullam
+            magni dolore dignissimos.
+          </p>
+        </div>
+
+        <div class="rows">
+          <div class="s s-two">
+            <img src="img/physics.svg" alt="" />
+          </div>
+          <h3>WEb DeVelOpmEnT</h3>
+          <h5>Start From <span>$99</span></h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+            maiores ipsum, neque quae quo velit similique perferendis, ullam
+            magni dolore dignissimos.
+          </p>
+        </div>
+
+        <div class="rows">
+          <div class="s s-three">
+            <img src="img/contact.svg" alt="" />
+          </div>
+          <h3>WEb DeVelOpmEnT</h3>
+          <h5>Start From <span>$99</span></h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+            maiores ipsum, neque quae quo velit similique perferendis, ullam
+            magni dolore dignissimos.
+          </p>
+        </div>
+
+        <div class="rows">
+          <div class="s s-four">
+            <img src="img/web.svg" alt="" />
+          </div>
+          <h3>WEb DeVelOpmEnT</h3>
+          <h5>Start From <span>$99</span></h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+            maiores ipsum, neque quae quo velit similique perferendis, ullam
+            magni dolore dignissimos.
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
