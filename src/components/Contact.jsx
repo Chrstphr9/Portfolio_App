0e35354075c8b4f18ba39274a7e8e064eed0a04f
Chstphr <somto565@gmail.com>
2023-03-30 22:52:56 +0200

    done with contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,6 +8,13 @@ const Contact = () => {
         <h3>Let's Talk About Everything</h3>
           <p>Don't Like Forms? Send Me An Email</p>
         </div>
+
+        <div className="action">
+          <form>
+          <input type="email" name="email" placeholder="Enter Your Email" required />
+          <input type="submit" name="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     </section>
   )
