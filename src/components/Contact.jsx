545aa3eb2174fd6f5f1b86a9df9e7a74da896b6a
Chstphr <somto565@gmail.com>
2023-03-30 22:39:18 +0200

    contact space

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <section></section>
+    <section class='contact' id='contact'>
+      <div className="container">
+        <div className="center">
+        <h3>Let's Talk About Everything</h3>
+          <p>Don't Like Forms? Send Me An Email</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
